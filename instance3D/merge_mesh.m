a327f47c82a25594f9fe767daf69ee504b344583
Lucas Brynte <brynte@chalmers.se>
2018-01-25 18:48:49 +0100

    Replace cell arrays of structs with non-scalar structs

--- instance3D/merge_mesh.m
+++ instance3D/merge_mesh.m
@@ -1,8 +1,8 @@
-function [tri, Utri] = merge_mesh(tricell, Utricell)
+function [tri, Utri] = merge_mesh(mesh_data)
     tri = zeros(3,0);
     Utri = zeros(3,0);
-    for j = 1:length(tricell)
-        tri = [tri,tricell{j}+size(Utri,2)];
-        Utri = [Utri,Utricell{j}];
+    for j = 1:length(mesh_data)
+        tri = [tri,mesh_data(j).tri+size(Utri,2)];
+        Utri = [Utri,mesh_data(j).U];
     end
 end
