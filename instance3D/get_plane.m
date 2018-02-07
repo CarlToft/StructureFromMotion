d8d3181d3600c3df0481fe4ce08c8a2b8b37002e
Lucas Brynte <brynte@chalmers.se>
2018-02-07 22:41:07 +0100

    Moved saftsoppa code into separate functions. R emains to annotate them.

--- instance3D/get_plane.m
+++ instance3D/get_plane.m
@@ -1,9 +1,11 @@
 % Returns parameters for a particular plane in bookshelf
-function [plane] = get_plane(Ushelf, plane_idx)
+function [plane, front_direction] = get_plane(Ushelf, plane_idx)
     if plane_idx == 1
         pt1 = 25;
         pt2 = 26;
         pt3 = 27;
+        pt4 = 12;
+        pt5 = 14;
     else
         error('Only top plane (plane_idx==1) implemented');
     end
@@ -13,4 +15,8 @@ function [plane] = get_plane(Ushelf, plane_idx)
     n = cross(v1,v2);n= n/norm(n);
 
     plane = [n;-n'*Ushelf(:,pt1)];
+
+    v3 = Ushelf(:,pt5)-Ushelf(:,pt4);
+    v4 = Ushelf(:,pt3)-Ushelf(:,pt2);
+    front_direction = cross(v3,v4);front_direction=front_direction/norm(front_direction);
 end
--- instance3D/test_annotate_exp_gopro1830.m
+++ instance3D/test_annotate_exp_gopro1830.m
@@ -68,6 +68,17 @@ for j = 1:nbr_instances
 end
 
 
+% Add points for saftsoppa on mid-upper shelf.
+% [u_uncalib,U]=add_1point(settings,P_uncalib,U,u_uncalib,[210,220,230,290]);
+load result3a_mid_upper_saftsoppa
+lookups.midupper_shelf_saftsoppa = struct('U_idx', {});
+nbr_instances = 9;
+for j = 1:nbr_instances
+    idx = size(U,2)-nbr_instances + j;
+    lookups.midupper_shelf_saftsoppa(j) = create_lookups(U, u_uncalib, 1, idx);
+end
+
+
 
 mesh = struct();
 
@@ -82,33 +93,6 @@ mesh.shelves = struct('tri', {}, 'U', {});
 %add bookshelf 3
 % mesh.shelves(3) = struct();
 [mesh.shelves(3).tri, mesh.shelves(3).U] = annotate_addshelf(U(1:3, lookups.shelves(3).U_idx), [2,6,1,25,22]);
-
-
-% %blue berry covers
-% tribox = [1,2,3;1,3,4;3,4,7;4,7,8;5,6,7;5,7,8;5,8,9;10,11,12;11,13,12;7,8,12;8,12,13]';
-% 
-% Utmp = U(1:3,nbrpoints_sfm+[20:31]);
-% %small fix of one point
-% v1=U(1:3,nbrpoints_sfm + 29)-U(1:3,nbrpoints_sfm + 30);v2=U(1:3,nbrpoints_sfm + 31)-U(1:3,nbrpoints_sfm + 30);n=cross(v1,v2);n=n/norm(n);
-% Utmp(:,11)=Utmp(:,11)+0.00008*n;
-% 
-% 
-% 
-% 
-% v1 = Utmp(:,11)-Utmp(:,10);
-% v2 = Utmp(:,12)-Utmp(:,10);
-% Utmp = [Utmp,(Utmp(:,11)+v2+Utmp(:,12)+v1)/2];
-% tri = [tri,tribox+size(Utri,2)];
-% Utri = [Utri,Utmp];
-% 
-% %coca cola cans cover
-% tribox = [1,2,3;1,3,4;3,4,6;4,5,6;5,6,7;5,7,8]';
-% 
-% Utmp = U(1:3,nbrpoints_sfm+[32:39]);
-% tri = [tri,tribox+size(Utri,2)];
-% Utri = [Utri,Utmp];
-
-
 
 
 
@@ -153,186 +137,30 @@ end
 
 
 
+% 9 boxes of saftsoppa
+[plane, front_direction] = get_plane(mesh.shelves(2).U, 1);
+for j=1:length(lookups.midupper_shelf_saftsoppa),
+    instance_cnt=instance_cnt+1;
+    lid_position = U(1:3, lookups.midupper_shelf_saftsoppa(j).U_idx);
+    [mesh.midupper_shelf_saftsoppa(j).tri, mesh.midupper_shelf_saftsoppa(j).U] = annotate_addsaftsoppa(plane, lid_position, front_direction);
+    mesh.midupper_shelf_saftsoppa(j).class_label = 6;
+end
+
 % Merge mesh
 [tri, Utri] = merge_mesh(mesh.shelves);
 
 all_instances = [ ...
     mesh.right_shelf_bottles, ...
-    mesh.left_shelf_bottles];
+    mesh.left_shelf_bottles, ...
+    mesh.midupper_shelf_saftsoppa];
 
 [triobj, Uobj, instance_labels] = merge_mesh(all_instances);
 class_labels = [all_instances.class_label];
 
 
+
 % THE FOLLOWING IS OLD
 STOP_HERE
-
-
-
-%saft soppa
-alpha = 0:0.4:2*pi;rr = 1.5;height=1;nn=length(alpha);
-cc = [3.75,2.0];
-
-th = atan(1.5/7.0);
-Usaft=[rr*cos(alpha),rr*cos(alpha),0;rr*sin(alpha),rr*sin(alpha),0;zeros(size(alpha)),height*ones(size(alpha)),1];
-ccindex = size(Usaft,2);
-
-Usaft(1,:)=Usaft(1,:)+cc(1);
-Usaft(2,:)=Usaft(2,:)+cc(2);
-
-R = [1,0,0;0,cos(th),sin(th);0,-sin(th),cos(th)];
-
-Usaft = R'*Usaft;
-trisaft = [1:nn,      nn+1:2*nn, 2*nn+1*ones(1,nn) ; ...
-             [2:nn,1],  [nn+2:2*nn,nn+1]  , nn+1:2*nn, ;...
-             nn+1:2*nn, [2:nn,1]       , [nn+2:2*nn,nn+1]];
-         
-Uframe = [0,0,0;7.5,0,0;7.5,7.0,1.5;0,7.0,1.5;0,0,-18.8;7.5,0,-18.8;7.5,7.0,-18.8;0,7.0,-18.8]';
-trisaft(:,end+[1:12])=size(Usaft,2)+[1,2,3;1,3,4;5,6,7;5,7,8;1,2,5;2,5,6;2,3,7;2,7,6;3,4,7;4,7,8;1,5,8;1,8,4]';
-Usaft = [Usaft,Uframe];
-Ucc = Usaft(:,ccindex);
-
-
-%plane of saft soppa
-v1 = mesh.shelves(2).U(:,26)-mesh.shelves(2).U(:,25);
-v2 = mesh.shelves(2).U(:,27)-mesh.shelves(2).U(:,26);
-v3 = mesh.shelves(2).U(:,27)-mesh.shelves(2).U(:,26);
-v4 = mesh.shelves(2).U(:,14)-mesh.shelves(2).U(:,12);
-
-n = cross(v1,v2);n= n/norm(n);
-n2 = cross(v3,v4);n2= n2/norm(n2);
-
-%n = n + 0.05*n2;n=n/norm(n);
-pl = [n;-n'*mesh.shelves(2).U(:,25)-0.0001];
-
-cc = 0;
-for ii=nbrpoints_anno+[16:30],
-    Upp = U(1:3,ii);
-    instance_cnt=instance_cnt+1;
-    cc = cc +1;
-    if cc==5,
-        cc=1;
-    end
-    if cc<=8,
-        class_labels(instance_cnt) = 5; %blackberry
-    else
-        class_labels(instance_cnt) = 4; %blueberry
-    end
-        
-    ll = -n'*Upp-pl(4);
-    Uplane = Upp+ll*n;
-
-    sc = norm(Uplane-Upp)/norm(-18.8-Ucc(3));
-
-    R = [null(n'), n]; if det(R)<0,R=[R(:,2),R(:,1),R(:,3)];end
-    
-    tmp = R'*n2;
-    tmp(3)=0;tmp=tmp/norm(tmp);
-    th2 = atan2(tmp(1),tmp(2))+3*pi/2;
-    R = R*[cos(th2),sin(th2),0;-sin(th2),cos(th2),0;0,0,1];
-    
-    tt = Uplane - sc*R*[3.75;cos(th)*2.0;-18.8];
-    T = [sc*R,tt;[0,0,0,1]];
-
-    Utmp=T(1:3,:)*pextend(Usaft);
-
-    triobj = [triobj,trisaft+size(Uobj,2)];
-    Uobj=[Uobj,Utmp];
-    instance_labels=[instance_labels,instance_cnt*ones(1,size(trisaft,2))];
-end
-
-
-
-%three packages in random position
-pl = [n;-n'*mesh.shelves(2).U(:,25)];
-class_labels = [class_labels,4,5,5];
-for ii=nbrpoints_anno+[31:2:36],
-    Upp1 = U(1:3,ii);
-    Upp2 = U(1:3,ii+1);
-    jj=(ii-nbrpoints_anno-31)/2;
-    Upp = U(1:3,nbrpoints_anno+37+jj);
-    
-    if ii==nbrpoints_anno+31,
-        nn = Upp2-Upp1;
-        Upp = Upp+0.015*nn;
-    end
-    
-    
-    instance_cnt=instance_cnt+1;
-        
-    ll = -n'*Upp-pl(4);
-    Uplane = Upp+ll*n;
-    
-    ll1 = -n'*Upp1-pl(4);
-    Uplane1 = Upp1+ll1*n;
-    
-    ll2 = -n'*Upp2-pl(4);
-    Uplane2 = Upp2+ll2*n;
-
-    sc = norm(Uplane-Upp)/norm(-18.8-Ucc(3));
-
-    R = [null(n'), n]; if det(R)<0,R=[R(:,2),R(:,1),R(:,3)];end
-    n2ii=cross(Upp1-Upp2,n);n2ii=n2ii/norm(n2ii);
-    tmp = R'*n2ii;
-    tmp(3)=0;tmp=tmp/norm(tmp);
-    th2 = atan2(tmp(1),tmp(2));
-    
-    if ii==nbrpoints_anno+31,
-        th2 = th2 + 3*pi/180;
-    end
-    R = R*[cos(th2),sin(th2),0;-sin(th2),cos(th2),0;0,0,1];
-    
-    tt = Uplane - sc*R*[3.75;cos(th)*2.0;-18.8];
-%    tt = Uplane1 - sc*R*[0,7.0,-18.8]';
-    T = [sc*R,tt;[0,0,0,1]];
-
-    Utmp=T(1:3,:)*pextend(Usaft);
-
-    triobj = [triobj,trisaft+size(Uobj,2)];
-    Uobj=[Uobj,Utmp];
-    instance_labels=[instance_labels,instance_cnt*ones(1,size(trisaft,2))];
-end
-
-%three packages in random position
-pl = [n;-n'*mesh.shelves(2).U(:,1)-0.0001];
-class_labels = [class_labels,4,5,5];
-for ii=nbrpoints_anno+[40:3:45],
-    Upp1 = U(1:3,ii);
-    Upp2 = U(1:3,ii+1);
-    Upp = U(1:3,ii+2);
-    
-    instance_cnt=instance_cnt+1;
-        
-    ll = -n'*Upp-pl(4);
-    Uplane = Upp+ll*n;
-    
-    ll1 = -n'*Upp1-pl(4);
-    Uplane1 = Upp1+ll1*n;
-    
-    ll2 = -n'*Upp2-pl(4);
-    Uplane2 = Upp2+ll2*n;
-
-    sc = norm(Uplane-Upp)/norm(-18.8-Ucc(3));
-
-    R = [null(n'), n]; if det(R)<0,R=[R(:,2),R(:,1),R(:,3)];end
-    n2ii=cross(Upp1-Upp2,n);n2ii=n2ii/norm(n2ii);
-    tmp = R'*n2ii;
-    tmp(3)=0;tmp=tmp/norm(tmp);
-    th2 = atan2(tmp(1),tmp(2));
-    
-    R = R*[cos(th2),sin(th2),0;-sin(th2),cos(th2),0;0,0,1];
-    
-    tt = Uplane - sc*R*[3.75;cos(th)*2.0;-18.8];
-    T = [sc*R,tt;[0,0,0,1]];
-
-    Utmp=T(1:3,:)*pextend(Usaft);
-
-    triobj = [triobj,trisaft+size(Uobj,2)];
-    Uobj=[Uobj,Utmp];
-    instance_labels=[instance_labels,instance_cnt*ones(1,size(trisaft,2))];
-end
-
-
 
 
 
